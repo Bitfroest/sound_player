8f9db231dd974148864835026e8b8ebc20232072
Fabian Schurig <fabian.schurig.94@gmail.com>
2015-10-23 14:43:03 +0200

    sounds

--- include/sound_player.h
+++ include/sound_player.h
@@ -2,15 +2,28 @@
 #define SOUND_PLAYER_H
 
 #include <lms/datamanager.h>
+#include <lms/module.h>
+#include <lms/type/module_config.h>
+#include <SFML/Audio.hpp>
 
 class SoundPlayer : public lms::Module {
 public:
     bool initialize() override;
     bool cycle() override;
     bool deinitialize() override;
+    struct SoundBufferContainer {
+        sf::SoundBuffer buffer;
+        sf::Sound sound;
+        std::string name;
+        int rpm;
+    };
 private:
-    const Gamepad *gamepad;
     const lms::type::ModuleConfig *config;
+    std::vector<SoundBufferContainer> sounds;
+    std::vector<sf::SoundBuffer> buffers;
+    float k = 0;
+    int buffersize;
+    std::vector<int> loudness;
 };
 
 #endif /* SOUND_PLAYER_H */
--- src/sound_player.cpp
+++ src/sound_player.cpp
@@ -1,26 +1,72 @@
 #include <sound_player.h>
-#include <gamepad/gamepad_types.h>
 #include <lms/extra/time.h>
-#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <lms/framework.h>
 
 bool SoundPlayer::initialize() {
-    gamepad = datamanager()
-                ->readChannel<Gamepad>(this, "GAMEPAD");
-    config = getConfig();
 
+    config = getConfig();
+    std::vector<std::string> motorsounds = config->getArray<std::string>("motorsound");
+    std::vector<std::string> rpm = config->getArray<std::string>("rpm");
+
+    buffersize = motorsounds.size();
+    sounds.resize(buffersize);
+    buffers.resize(buffersize);
+    loudness.resize(buffersize);
+
+
+    for(int i = 0; i < buffersize;i++){
+        SoundBufferContainer container;
+        if(!buffers.at(i).loadFromFile(lms::Framework::configsDirectory + "/" +motorsounds[i]))
+            return false;
+        container.buffer = buffers.at(i);
+        container.sound.setBuffer(buffers.at(i));
+        container.sound.setVolume(100);
+        container.sound.setLoop(true);
+        container.sound.setPitch(1.0);
+        container.sound.play();
+        container.name = motorsounds[i];
+        container.rpm = atoi(rpm[i].c_str());
+        sounds.at(i) = container;
+        sounds.at(i).sound.setVolume(0);
+        sounds.at(i).sound.play();
+
+    }
     return true;
 }
 
 bool SoundPlayer::cycle() {
-    Gamepad::axis ls = gamepad->getAxis("LS");
-    Gamepad::axis rs = gamepad->getAxis("RS");
-    Gamepad::axis lt = gamepad->getAxis("LT");
-    Gamepad::axis rt = gamepad->getAxis("RT");
-    bool ba = gamepad->buttonPressed("A");
-    bool bb = gamepad->buttonPressed("B");
-    bool bx = gamepad->buttonPressed("X");
-    bool by = gamepad->buttonPressed("Y");
 
+    for(int i = 0; i < sounds.size(); i++) {
+        logger.info("cycle") <<  sounds.at(i).name << " | status: " << sounds.at(i).sound.getStatus() << " | volume: " << sounds.at(i).sound.getVolume();
+    }
+    k++;
+
+
+    for(int i = 0; i< buffersize;i++){
+        float factor =  (  float (k) / (sounds.at(i).rpm * 60));
+        logger.info("cycle") << factor;
+        if (factor <=0 && factor <= 0.9){
+            loudness.at(i) = 0;
+            sounds.at(i).sound.setVolume(0);
+        } else if (factor > 0.9 && factor < 1 && loudness.at(i) < 100){
+            if(cycleCounter() % 120 == 0)
+                loudness.at(i)++;
+            sounds.at(i).sound.setPitch(factor);
+            sounds.at(i).sound.setVolume(loudness.at(i));
+        } else if (factor >= 1 && factor <= 1.8){
+            sounds.at(i).sound.setPitch(factor);
+            sounds.at(i).sound.setVolume(100);
+            loudness.at(i) = 100;
+        } else if(factor >=1.8 && factor <= 2 && loudness.at(i) > 0){
+            if(cycleCounter() % 50 == 0)
+                loudness.at(i)--;
+            sounds.at(i).sound.setPitch(factor);
+            sounds.at(i).sound.setVolume(loudness.at(i));
+        }else{
+            sounds.at(i).sound.setVolume(0);
+        }
+    }
 
     return true;
 }
